f5c2c023b051dfc4ae58b6fba81b449262b83945
The King Lion <95191722+thekinglion15@users.noreply.github.com>
2024-01-30 21:06:53 -0400

    Add files via upload

---
+++ ProgramaSocio/ProgramaSocio/Club.cs
@@ -0,0 +1,43 @@
+﻿namespace ProgramaSocio
+{
+    public class Club
+    {
+        private Member member1;
+        private Member member2;
+        private Member member3;
+
+        public Club(Member member1, Member member2, Member member3)
+        {
+            this.member1 = member1;
+            this.member2 = member2;
+            this.member3 = member3;
+        }
+
+        public void MemberMoreAntiquity()
+        {
+            Member memberMoreAntiquity = null;
+
+            if (member1.Antiquity > member2.Antiquity && member1.Antiquity > member3.Antiquity)
+            {
+                memberMoreAntiquity = member1;
+            }
+            else if (member2.Antiquity > member1.Antiquity && member2.Antiquity > member3.Antiquity)
+            {
+                memberMoreAntiquity = member2;
+            }
+            else
+            {
+                memberMoreAntiquity = member3;
+            }
+
+            if(memberMoreAntiquity != null)
+            {
+                Console.WriteLine($"The member with the highest antiquity is: {memberMoreAntiquity.Name}");
+            }
+            else
+            {
+                Console.WriteLine("There aren't members in the club.");
+            }
+        }
+    }
+}
---
+++ ProgramaSocio/ProgramaSocio/Program.cs
@@ -0,0 +1,19 @@
+﻿using ProgramaSocio;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the member 1:");
+        Member member1 = Member.NewMember();
+
+        Console.WriteLine("Enter the member 2:");
+        Member member2 = Member.NewMember();
+
+        Console.WriteLine("Enter the member 3:");
+        Member member3 = Member.NewMember();
+
+        Club club = new Club(member1, member2, member3);
+        club.MemberMoreAntiquity();
+    }
+}
